1db287ad3a5ae2d7cd14b638de4b1f18f6c53b3a
Asahi20 <asahiyamaguchi1120@gmail.com>
2021-07-17 17:26:40 +0900

    newまで完了。new,index,showページの表示確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -19,5 +27,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age, :email)
   end
 end

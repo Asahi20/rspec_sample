23125f67cf28546000472693c432270136fa95b7
Asahi20 <asahiyamaguchi1120@gmail.com>
2021-07-19 23:11:43 +0900

    実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user
+    else
+      render :edit
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    redirect_to user
   end
 
   private
